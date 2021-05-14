aef619c624b936e4adf3b9f14e4fc3ddc8c9cd7a
ImHyeLim1209 <ihl279090@gmail.com>
2021-05-14 23:16:57 +0900

    create crud

---
+++ src/movies/entities/movie.entity.ts
@@ -0,0 +1,7 @@
+//보통 Database Model이 여기에 있음
+export class Movie {
+  id: number;
+  title: string;
+  year: number;
+  genres: string[];
+}
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,37 +1,41 @@
 //라우팅. 여기서 바로 'hello'를 리턴해도 되지만 service에서 하는 것을 권장
 //controller의 함수와 service의 함수명은 똑같이 하는 것을 권장
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
   @Get('')
-  getAll() {
-    return 'This will return all movies';
+  getAll() :Movie[] {
+    return this.moviesService.getAll();
   }
 
-  @Get('/search')
-  search(@Query('year') searchingYear:string) {
-    return 'we are searching for a movie with title';
-  }
+  // @Get('/search')
+  // search(@Query('year') searchingYear:string) {
+  //   return 'we are searching for a movie with title';
+  // }
 
   @Get('/:id')
-  getOne(@Param('id') movieId: string) {
-    return `This will return one movie ${movieId}`;
+  getOne(@Param('id') movieId: string) :Movie {
+    return this.moviesService.getOne(movieId);
   }
 
   @Post()
   create(@Body() movieData) {
-    return 'This will create movie';
+    return this.moviesService.create(movieData);
   }
 
   @Delete('/:id')
-  delete(@Param('id') movieId:string) {
-    return `This will remove a movie ${movieId}`;
+  remove(@Param('id') movieId:string) {
+    return this.moviesService.deleteOne(movieId);
   }
 
   @Patch('/:id')
   patch(@Param('id') movieId:string, @Body() updateData) {
-    return `This will patch a movie ${movieId}`;
+    return this.moviesService.update(movieId, updateData);
   }
 }
 
