bd8688bebf4544682b488a112347c4fda15a97f3
ImHyeLim1209 <ihl279090@gmail.com>
2021-05-14 20:51:50 +0900

    remove controller-service

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+
+//데코레이터: 클래스에 함수 기능 추가
+//AppModule의 진짜 핵심 내용을 아래 데코레이터 내용이다!
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
